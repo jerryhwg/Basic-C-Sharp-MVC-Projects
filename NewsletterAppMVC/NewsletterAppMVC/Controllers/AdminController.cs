e7492a2652c9966435913a0e0ab4506b43f1c35c
Jerry Hwang <jerryhwang@outlook.com>
2018-12-13 22:11:15 -0800

    Update

--- NewsletterAppMVC/NewsletterAppMVC/Controllers/AdminController.cs
+++ NewsletterAppMVC/NewsletterAppMVC/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
-﻿using NewsletterAppMVC.ViewModels;
+﻿using NewsletterAppMVC.Models;
+using NewsletterAppMVC.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,11 +16,12 @@ namespace NewsletterAppMVC.Controllers
             // The following shows a simple line replaces the detailed db connection (ADO.NET) by using EF (EntityFramework)
             using (NewsletterEntities db = new NewsletterEntities()) // use db oject to access database / NewsletterEntities from Model1.Context.cs for databasse connection / a best practice to use 'using' to cut off the db connection when done
             {
-                var signups = db.SignUps; // SignUps: property (see Model1.Context.cs) which represents all of records in the database
+                var signups = db.SignUps.Where (x => x.Removed == null).ToList(); // SignUps: property (see Model1.Context.cs) which represents all of records in the database
                 var signupVms = new List<SignupVm>(); // if it's obvious what the data type is, you don't list it twice, use var instead of List<SignupVm>
                 foreach (var signup in signups)
                 {
                     var signupVm = new SignupVm();
+                    signupVm.Id = signup.Id;
                     signupVm.FirstName = signup.FirstName;
                     signupVm.LastName = signup.LastName;
                     signupVm.EmailAddress = signup.EmailAddress;
@@ -28,6 +30,17 @@ namespace NewsletterAppMVC.Controllers
 
                 return View(signupVms); // ViewModels (Model maps to ViewModels)
             }
+        }
+
+        public ActionResult Unsubscribe(int Id)
+        {
+            using (NewsletterEntities db = new NewsletterEntities())
+            {
+                var signup = db.SignUps.Find(Id);
+                signup.Removed = DateTime.Now;
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
         }
     }
 }
