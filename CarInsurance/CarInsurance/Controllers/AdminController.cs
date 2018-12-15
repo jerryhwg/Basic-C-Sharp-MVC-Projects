3b4efd53dfdd8893d989efe777ac0a11f5e98a66
Jerry Hwang <jerryhwang@outlook.com>
2018-12-14 19:08:54 -0800

    Base

---
+++ CarInsurance/CarInsurance/Controllers/AdminController.cs
@@ -0,0 +1,34 @@
+﻿using CarInsurance.Models;
+using CarInsurance.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarInsurance.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            using (CarInsuranceEntities db = new CarInsuranceEntities())
+            {
+                var QuoteForms = db.QuoteForms; // 243:2:32
+                var quoteFormVms = new List<QuoteFormVm>();
+                foreach (var quote in QuoteForms)
+                {
+                    var quoteVm = new QuoteFormVm();
+                    quoteVm.Id = quote.Id;
+                    quoteVm.FirstName = quote.FirstName;
+                    quoteVm.LastName = quote.LastName;
+                    quoteVm.EmailAddress = quote.EmailAddress;
+                    quoteFormVms.Add(quoteVm);
+                }
+
+                return View(quoteFormVms);
+            }
+        }
+    }
+}
---
+++ CarInsurance/CarInsurance/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using CarInsurance.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarInsurance.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult QuoteForm(string firstName, string lastName, string emailAddress, int carYear, string carMake, string carModel, int ticketNumber)
+        {
+            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName) || string.IsNullOrEmpty(emailAddress) || string.IsNullOrEmpty(carYear.ToString()) || string.IsNullOrEmpty(carMake) || string.IsNullOrEmpty(carModel) || string.IsNullOrEmpty(ticketNumber.ToString()))
+            {
+                return View("~/Views/Shared/Error.cshtml");
+            }
+            else
+            {
+                using (CarInsuranceEntities db = new CarInsuranceEntities())
+                {
+                    var quote = new QuoteForm();
+                    quote.FirstName = firstName;
+                    quote.LastName = lastName;
+                    quote.EmailAddress = emailAddress;
+                    quote.CarYear = carYear;
+                    quote.CarMake = carMake;
+                    quote.CarModel = carModel;
+                    quote.TicketNumber = ticketNumber;
+
+                    db.QuoteForms.Add(quote); // 242:6:42, Models -> CarInsurance.Context.tt -> CarInsurance.Context.cs
+                    db.SaveChanges();
+                }
+                return View("Success");
+            }
+        }
+    }
+}
